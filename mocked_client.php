721ffb9a545051179fa0c7a356b3141481f937ae
Igor Iakovlev <igor.iakovlev@igms.com>
2025-11-02 20:49:14 +0400

    Added mock client builder;

--- mocked_client.php
+++ mocked_client.php
@@ -1,41 +1,23 @@
 <?php
 
 use App\FbaShippingService;
-use GuzzleHttp\Client;
+use App\Support\MockClientBuilder;
 use GuzzleHttp\ClientInterface;
-use GuzzleHttp\Handler\MockHandler;
-use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Psr7\Response;
+use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\RateLimiter\RateLimiterFactory;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use function DI\autowire;
 use function DI\create;
 use function DI\get;
 
 return [
-    'mock.client' => function (): ClientInterface {
-        $mock = new MockHandler([
-            New Response(200, [], json_encode([
-                'errors' => [],
-            ])),
-            new Response(200, [], json_encode([
-                'payload' => [
-                    'fulfillmentShipments' => [
-                        [
-                            'fulfillmentShipmentPackage' => [
-                                [
-                                    'trackingNumber' => '1Z999AA1234567890',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                'errors' => [],
-            ])),
-        ]);
-        $handlerStack = HandlerStack::create($mock);
-        return new Client(['handler' => $handlerStack, 'base_uri' => 'http://fake-fba.local/']);
+    MockClientBuilder::class => autowire(),
+    'mock.client' => function (ContainerInterface $di): ClientInterface {
+        /** @var MockClientBuilder $clientBuilder */
+        $clientBuilder = $di->get(MockClientBuilder::class);
+        return $clientBuilder->withSuccess()->build();
     },
     'fba.mock' => create(FbaShippingService::class)
         ->constructor(
---
+++ src/Support/MockClientBuilder.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace App\Support;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+
+class MockClientBuilder extends MockHandler
+{
+    /**
+     * @param string $sellerId
+     * @param string $tracking
+     * @return $this
+     */
+    public function withSuccess(string $sellerId = 'MCF-16400', string $tracking = '1Z999AA1234567890'): self
+    {
+        $this->append(new Response(
+            200,
+            ['Content-Type' => 'application/json'],
+            $this->createSuccessPayload()
+        ));
+
+        $this->append(new Response(
+            200,
+            ['Content-Type' => 'application/json'],
+            $this->getFulfillmentOrderPayload($sellerId, $tracking)
+        ));
+
+        return $this;
+    }
+
+    /**
+     * @param string $sellerId
+     * @param string $tracking
+     * @return $this
+     */
+    public function withPartialError(string $sellerId = 'MCF-16400', string $tracking = '1Z999AA1234567890'): self
+    {
+        $this->append(new Response(
+            200,
+            ['Content-Type' => 'application/json'],
+            $this->createPartialErrorPayload()
+        ));
+
+        $this->append(new Response(
+            200,
+            ['Content-Type' => 'application/json'],
+            $this->getFulfillmentOrderPayload($sellerId, $tracking)
+        ));
+        return $this;
+    }
+
+    /**
+     * @param string $sellerId
+     * @return $this
+     */
+    public function withErrorStatus(string $sellerId = 'MCF-16400'): self
+    {
+        $this->append(new Response(
+            200,
+            ['Content-Type' => 'application/json'],
+            $this->createSuccessPayload()
+        ));
+
+        $this->append(new Response(
+            400,
+            ['Content-Type' => 'application/json'],
+            json_encode([
+                'errors' => [
+                    ['code' => 'InvalidInput', 'message' => 'Order not found or invalid status'],
+                ],
+        ])));
+
+        return $this;
+    }
+
+    /**
+     * @param string $sellerId
+     * @return $this
+     */
+    public function withoutTracking(string $sellerId = 'MCF-16400'): self
+    {
+        $this->append(new Response(
+            200,
+            ['Content-Type' => 'application/json'],
+            $this->createSuccessPayload()
+        ));
+
+        $this->append(new Response(
+            200,
+            ['Content-Type' => 'application/json'],
+            $this->getFulfillmentOrderPayload($sellerId, null, true)
+        ));
+
+        return $this;
+    }
+
+
+    /**
+     * @return Client
+     */
+    public function build(): Client
+    {
+        $handlerStack = HandlerStack::create($this);
+        return new Client(['handler' => $handlerStack, 'base_uri' => 'http://fake-fba.local/']);
+    }
+
+    /**
+     * @return string
+     */
+    private function createSuccessPayload(): string
+    {
+        return json_encode([
+            'payload' => [
+                'fulfillmentOrder' => ['sellerFulfillmentOrderId' => 'MCF-16400', 'fulfillmentOrderStatus' => 'Processing'],
+                'fulfillmentOrderItems' => [['sellerSku' => 'SKU1', 'quantity' => ['value' => 1]]],
+            ],
+            'errors' => [],
+        ]);
+    }
+
+    /**
+     * @return string
+     */
+    private function createPartialErrorPayload(): string
+    {
+        return json_encode([
+            'payload' => [
+                'fulfillmentOrder' => ['sellerFulfillmentOrderId' => 'MCF-16400', 'fulfillmentOrderStatus' => 'Processing'],
+                'fulfillmentOrderItems' => [
+                    ['sellerSku' => 'SKU1', 'quantity' => ['value' => 1]],
+                    ['sellerSku' => 'INVALID-SKU', 'unfulfillableQuantity' => ['value' => 1]],
+                ],
+            ],
+            'errors' => [
+                ['code' => 'SkuNotFound', 'message' => 'SKU INVALID-SKU not in inventory'],
+            ],
+        ]);
+    }
+
+    /**
+     * @param string $sellerId
+     * @param string|null $tracking
+     * @param bool $noPackages
+     * @return string
+     */
+    private function getFulfillmentOrderPayload(
+        string $sellerId,
+        ?string $tracking = null,
+        bool $noPackages = false
+    ): string {
+        $shipments = [
+            [
+                'fulfillmentShipmentId' => 'AMZ-SH-123',
+                'shipmentStatus' => 'Shipped',
+                'fulfillmentShipmentPackage' => $noPackages ? [] : [
+                    ['packageNumber' => 1, 'trackingNumber' => $tracking],
+                ],
+            ],
+        ];
+
+        return json_encode([
+            'payload' => [
+                'fulfillmentOrder' => ['sellerFulfillmentOrderId' => $sellerId, 'fulfillmentOrderStatus' => 'Shipped'],
+                'fulfillmentShipments' => $shipments,
+            ],
+            'errors' => [],
+        ]);
+    }
+}
